a46d03fdf389a6db4ed5c08977b7f11375e402ed
Carmen Soriano <43436495+7-carmen@users.noreply.github.com>
2020-02-22 16:48:16 +0100

    Update herokuDB.sh

--- herokuDB.sh
+++ herokuDB.sh
@@ -4,7 +4,7 @@
 ##### Edit files
 
 sed -i 's@localhost:3306/Sample@'"$jdbcURL"'@g' src/main/resources/META-INF/persistence.xml
-sed -i 's/acme-user/'$jdbcUser'/g' src/main/resources/META-INF/persistence.xml
+sed -i 's/acme-manager/'$jdbcUser'/g' src/main/resources/META-INF/persistence.xml
 sed -i 's/ACME-M@n@ger-6874/'$jdbcPassword'/g' src/main/resources/META-INF/persistence.xml
 
 sed -i 's@localhost:3306/Sample@'$jdbcURL'@g' src/main/resources/spring/config/data.xml
